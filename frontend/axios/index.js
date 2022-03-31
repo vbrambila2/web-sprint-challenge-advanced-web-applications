10f5c06bd356b732688394f6d0b799811163d422
Victor Brambila <victorbrambila22@gmail.com>
2022-03-31 11:50:17 -0700

    set up axiosWithAuth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,11 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios';
+
+export default function axiosWithAuth() {
+    const token = window.localStorage.getItem('token');
+    return axios.create({
+        headers: {
+            Authorization: token
+        }
+    })
+}
